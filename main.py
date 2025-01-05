ecc1c3061b8ecf435bb6156d7ef3ea44230ffb22
aishwinj04 <aishwin@gmail.com>
2025-01-05 00:04:07 -0500

    Support for mp3 files

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from speech_recognition import Recognizer, AudioFile
+from pydub import AudioSegment
 
-def process_audio(audiofile):
+# given an mp3 file convert to wav for speech recognition
+def mp3_to_wav(audiofile):
+    audio = AudioSegment.from_mp3(audiofile) 
+    wav_file = f'{audiofile}.wav'
+    audio.export(wav_file, format='wav')
+
+    return wav_file
+
+def transcribe(wav_file):
     recognizer = Recognizer()
 
     # handle resource management 
-    with AudioFile(audiofile) as af:
+    with AudioFile(wav_file) as af:
         audio = recognizer.record(af)
     
     # transcribe using Google's web speech api
@@ -12,6 +21,3 @@ def process_audio(audiofile):
 
     return text
 
-
-
-        
